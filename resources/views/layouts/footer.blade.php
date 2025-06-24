7f897d02acbda36726388164b2b38a2160670e60
dungcaph51644 <dungcaph51644@gmail.com>
2025-06-24 19:32:18 +0700

    giaodienClient

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,74 @@
+<footer id="colophon" class="site-footer light text-dark" role="contentinfo">
+
+	<div class="footer-widgets widgets-area widgets-4-columns">
+		<div class="container">
+			<div class="row">
+
+				<div class="footer-widgets-area-1 footer-widgets-area col-xs-12 col-sm-6 col-md-4"><div id="text-4" class="widget widget_text">			<div class="textwidget"><p><a href=""><img decoding="async" class="alignnone size-full wp-image-71699" src="{{ asset('assets/images/1212.jpg') }}" alt="" width="984" height="337"></a></p>
+<p><strong>Hệ thống giày thể thao số 1 Hà Nội</strong></p>
+<p><span class="fa_head">Hotline</span> <a href="tel:0975671080">097.567.1080</a></p>
+<p><span class="fa_head">Store 1:</span> 57 Quan Hoa, Cầu Giấy, HN</p>
+<p><span class="fa_head">Store 2:</span> 29 Trần Đại Nghĩa, Hai Bà Trưng, HN</p>
+<p> </p>
+</div>
+		</div></div><div class="footer-widgets-area-2 footer-widgets-area col-xs-12 col-sm-6 col-md-2">
+		<div id="recent-posts-3" class="widget widget_recent_entries">
+		<h4 class="widget-title">Hỗ trợ</h4>
+		<ul>
+			<li><a href="#">7 cách bảo quản giày thể thao tốt nhất</a></li>
+			<li><a href="#">Giữ “phong độ” cho Sneaker trắng ra sao</a></li>
+			<li><a href="#">9 kỹ thuật làm đẹp dành cho U30</a></li>
+		</ul>
+		</div></div><div class="footer-widgets-area-3 footer-widgets-area col-xs-12 col-sm-6 col-md-2"><div id="nav_menu-2" class="widget widget_nav_menu"><h4 class="widget-title">THÔNG TIN</h4><div class="menu-thong-tin-container"><ul id="menu-thong-tin" class="menu">
+<li class="menu-item"><a href="#">Giới thiệu</a></li>
+<li class="menu-item"><a href="#">Hướng dẫn đặt hàng</a></li>
+<li class="menu-item"><a href="#">Chính sách đổi hàng</a></li>
+<li class="menu-item"><a href="#">Bảo mật</a></li>
+<li class="menu-item"><a href="#">Liên hệ</a></li>
+<li class="menu-item"><a href="#">Hệ thống cửa hàng</a></li>
+</ul></div></div></div><div class="footer-widgets-area-4 footer-widgets-area col-xs-12 col-sm-6 col-md-4"><div id="text-3" class="widget widget_text"><h4 class="widget-title">XSHOP trên Facebook</h4>
+<div class="textwidget">
+    {{-- Facebook Page Plugin --}}
+    <iframe loading="lazy" style="border: none; overflow: hidden;" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fxsh0p%2F&tabs=timeline&width=360&height=320&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=161233991086340" frameborder="0" scrolling="no"></iframe>
+</div>
+</div></div>
+			</div>
+		</div>
+	</div>
+
+	<div class="footer-info footer-full-width">
+		<div class="sober-container">
+			<div class="row">
+				<div class="site-info col-md-12">
+					<div class="copyright">
+						Bản quyền © 2018 - 2025 <a href="/">Giày xShop</a>. Bảo lưu mọi quyền.					</div>
+					<div class="extra-box">
+						<div><a href="#"><i class="fa fa-truck" aria-hidden="true"></i> Chính sách giao hàng</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer><div id="svg-defs" class="svg-defs hidden">
+    </div>
+
+<a href="#page" id="gotop">
+    <svg role="img" viewBox="0 0 20 20" width="20" height="20"> <use href="#backtotop-arrow" xlink:href="#backtotop-arrow"></use> </svg>
+    <span class="screen-reader-text">Go to top</span>
+</a>
+
+<div id="search-modal" class="search-modal sober-modal" tabindex="-1" role="dialog">
+    </div>
+
+<div id="login-modal" class="login-modal sober-modal woocommerce-account" tabindex="-1" role="dialog">
+    </div>
+
+<div id="cart-modal" class="cart-modal sober-modal woocommerce" tabindex="-1" role="dialog">
+    </div>
+
+<div id="quick-view-modal" class="quick-view-modal sober-modal woocommerce" tabindex="-1" role="dialog">
+    </div>
+
+<div class="side-menu-backdrop"></div>
+<div id="mobile-menu" class="side-menu mobile-menu has-bottom-content">
+    </div>
